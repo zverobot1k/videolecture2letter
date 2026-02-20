ca0413e10938bb1a41c342e1b521ef146c0aad50
sleepwalker <maksikkblohinn@gmail.com>
2026-02-20 11:34:21 +0300

    front-1.1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,46 @@
-import { useState } from 'react'
-import './App.css'
-import NeonBackground from "./NeonBackground";
-
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-  const [videoURL, setvideoURL] = useState("")
+    const [videoURL, setVideoURL] = useState("");
+    const [isCreated, setIsCreated] = useState(false);
 
-  return (
-    <div className="App">
-      <NeonBackground />
+    return (
+        <div className="screen">
+            <div className="card">
+                <h1>Создание конспекта</h1>
 
-      <div style={{ padding: "2rem", position: "relative", zIndex: 1 }}>
-        <h1>Конспект видео</h1>
+                <h3 className="subtitle">Вставьте ссылку</h3>
 
-        <input
-          type="text"
-          placeholder="Cсылка на лекцию"
-          value={videoURL}
-          onChange={(e) => setVideoURL(e.target.value)} // ✅ fixed
-          style={{ width: "300px", marginRight: "1rem" }}
-        />
+                <input
+                    type="text"
+                    placeholder="Ссылка на лекцию"
+                    value={videoURL}
+                    onChange={(e) => setVideoURL(e.target.value)}
+                />
 
-        <button>Создать конспект</button>
-      </div>
-    </div>
-  );
+                <button
+                    className="create-button"
+                    onClick={() => setIsCreated(true)}
+                >
+                    Сделать конспект
+                </button>
+
+                {isCreated && (
+                    <div className="result">
+                        <div>
+                            <p className="success">Конспект успешно создан!</p>
+                            <p className="title">Заголовок: Пример статьи</p>
+                        </div>
+
+                        <button className="download-button">
+                            Скачать файл
+                        </button>
+                    </div>
+                )}
+            </div>
+        </div>
+    );
 }
 
 export default App;
