3d961472a975566e8b76f38247171091e1230e21
sleepwalker <maksikkblohinn@gmail.com>
2026-03-14 16:49:08 +0300

    mb better fileResponce

--- backend/routes.py
+++ backend/routes.py
@@ -1,5 +1,8 @@
 from fastapi import APIRouter
+from fastapi.exceptions import HTTPException
+from fastapi.responses import FileResponse
 from backend.schemas import YoutubeRequest, TaskResponse
+from pathlib import Path
 from backend.redis_queue.tasks import enqueue_video, get_job_status
 
 router = APIRouter(prefix="/process", tags=["Summaries"])
@@ -9,7 +12,19 @@ async def process_video_route(data: YoutubeRequest):
     job_id = enqueue_video(data.url)
     return {"task_id": job_id, "status": "queued"}
 
-@router.get("/{task_id}", response_model=TaskResponse)
-async def get_task_status(task_id: str):
+@router.get("/{task_id}")
+async def get_task_file(task_id: str):
     status = get_job_status(task_id)
-    return {"task_id": task_id, "status": status["status"], "response": {summary_file}}
+    summary_file = Path(f"./results/{task_id}.txt")
+
+    if status["status"] != "done":
+        return {"task_id": task_id, "status": status["status"]}
+
+    if not summary_file.exists():
+        raise HTTPException(status_code=404, detail="File not found")
+
+    return FileResponse(
+        path=str(summary_file),
+        filename=summary_file.name,
+        media_type="text/plain"
+    )
